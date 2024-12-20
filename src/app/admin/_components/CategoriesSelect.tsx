0f93bf2ebed84891b5078dfc9c95ade09bf95391
iiiiirisa <tusen.takk39@gmail.com>
2024-12-21 06:03:28 +0900

    記事作成ページ ＿カテゴリーエラー

--- src/app/admin/_components/CategoriesSelect.tsx
+++ src/app/admin/_components/CategoriesSelect.tsx
@@ -13,7 +13,7 @@ interface Props {
   selectedCategories: Category[];
   //void は、この関数が何も返さない（戻り値がない）ことを示しています。
   //setSelectedCategories: 選択されたカテゴリーを更新するための関数
-  setSelectedCategories: (Categories: Category[]) => void;
+  setSelectedCategories: (categories: Category[]) => void;
 }
 
 export const CategoriesSelect: React.FC<Props> = ({
@@ -23,41 +23,42 @@ export const CategoriesSelect: React.FC<Props> = ({
   selectedCategories,
   setSelectedCategories,
 }) => {
-  console.log(selectedCategories);
   //React.useState<Category[]>([]): 初期値は空の配列で、カテゴリーのデータは後でサーバーから取得
-  const [categories, setCategories] =
-    React.useState<Category[]>(selectedCategories);
+  const [categories, setCategories] = React.useState<Category[]>([]);
+  console.log(selectedCategories);
 
-  //29-41行目は選択されたカテゴリーがすでにリストに含まれている場合は削除し、含まれていない場合は追加するように設計
-  //handleChange: ユーザーがカテゴリーを選択または解除したときに呼ばれる関数
-  //value: number[]:選択されたカテゴリーのIDの配列
-  const handleChange = (value: Category[]) => {
-    //value.forEach((v: number) => { ... })**: value の各IDについて処理を行います。
-    value.forEach((v) => {
-      //isSelect は、現在のID(=v) が selectedCategories に含まれているかどうかをチェック
-      //some method(JS): 配列の中に条件を満たす要素があるかどうかを確認。ここでは、選択されたカテゴリーの中に同じIDがあるかどうかをチェック
-      const isSelect = selectedCategories.some((c) => c.id === v.id);
-      if (isSelect) {
-        //もし isSelect が true なら、そのカテゴリーはすでに選択されているため、選択を解除
-        //filter　method(JS): 配列から条件を満たす要素だけを残します。ここでは、選択を解除するカテゴリーを除外
-        setSelectedCategories(selectedCategories.filter((c) => c.id !== v.id));
-        //returnで処理を終了するため、ユーザーが選択を解除した場合、それ以降の処理は行われません。
-        return;
-      }
-      //新たに選択された場合,find メソッド(JS)を使って、現在のID(=v)に対応するカテゴリーを categories の中から探します。
-      const category = categories.find((c) => c.id === v.id);
-      // もし category が見つからなかった場合、処理を終了します。
-      if (!category) return;
+  // 29-41行目は選択されたカテゴリーがすでにリストに含まれている場合は削除し、含まれていない場合は追加するように設計
+  // handleChange: ユーザーがカテゴリーを選択または解除したときに呼ばれる関数
+  // value: number[]:選択されたカテゴリーのIDの配列
 
-      //新たに選択されたカテゴリーを selectedCategories に追加します。
-      //...selectedCategories**: これは、`selectedCategories` の中にあるすべてのカテゴリーを展開します。
-      //例えば、`selectedCategories` が[Category1, Category2] という2つのカテゴリーを含んでいる場合、 `Category1` と Category2 を展開します。
-      //category**: これは、新たに選択された Category3 を指します。
-      //新しい配列 [Category1, Category2, Category3] を setSelectedCategories に渡すことで、 状態を更新し、選択されたカテゴリーのリストを最新のものにします
-      setSelectedCategories([...selectedCategories, category]);
-    });
-  };
+  // const handleChange = (value: Category[]) => {
+  //   //value.forEach((v: number) => { ... })**: value の各IDについて処理を行います。
+  //   value.forEach((v) => {
+  //     //isSelect は、現在のID(=v) が selectedCategories に含まれているかどうかをチェック
+  //     //some method(JS): 配列の中に条件を満たす要素があるかどうかを確認。ここでは、選択されたカテゴリーの中に同じIDがあるかどうかをチェック
+  //     const isSelect = selectedCategories.some((c) => c.id === v.id);
+  //     console.log(isSelect);
 
+  //     if (isSelect) {
+  //       //もし isSelect が true なら、そのカテゴリーはすでに選択されているため、選択を解除
+  //       //filter　method(JS): 配列から条件を満たす要素だけを残します。ここでは、選択を解除するカテゴリーを除外
+  //       setSelectedCategories(selectedCategories.filter((c) => c.id !== v.id));
+  //       //returnで処理を終了するため、ユーザーが選択を解除した場合、それ以降の処理は行われません。
+  //       return;
+  //     }
+  //     //新たに選択された場合,find メソッド(JS)を使って、現在のID(=v)に対応するカテゴリーを categories の中から探します。
+  //     const category = categories.find((c) => c.id === v.id);
+  //     // もし category が見つからなかった場合、処理を終了します。
+  //     if (!category) return;
+
+  //     //新たに選択されたカテゴリーを selectedCategories に追加します。
+  //     //...selectedCategories**: これは、`selectedCategories` の中にあるすべてのカテゴリーを展開します。
+  //     //例えば、`selectedCategories` が[Category1, Category2] という2つのカテゴリーを含んでいる場合、 `Category1` と Category2 を展開します。
+  //     //category**: これは、新たに選択された Category3 を指します。
+  //     //新しい配列 [Category1, Category2, Category3] を setSelectedCategories に渡すことで、 状態を更新し、選択されたカテゴリーのリストを最新のものにします
+  //     setSelectedCategories([...selectedCategories, category]);
+  //   });
+  // };
   useEffect(() => {
     const fetcher = async () => {
       const res = await fetch("/api/admin/categories");
@@ -67,30 +68,46 @@ export const CategoriesSelect: React.FC<Props> = ({
     fetcher();
   }, []);
 
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const selectedIds = event.target.value as number[]; // 選択されたIDの配列を取得
+    const newSelectedCategories = categories.filter((category) =>
+      selectedIds.includes(category.id)
+    );
+    setSelectedCategories(newSelectedCategories); // 新しい選択状態を更新
+  };
+
+  console.log(selectedCategories);
+
   return (
     //FormControl:MUIのコンポーネントで、フォーム要素をラップして一貫したスタイルを提供
     <FormControl className="w-full">
-      {/*Select:複数選択が可能なドロップダウンメニューを提供するMUIのコンポーネント*/}
+      {/* Select:複数選択が可能なドロップダウンメニューを提供するMUIのコンポーネント*/}
       <Select
         /*multiple:複数の項目を選択できるようになる */
         multiple
-        value={categories} // IDの配列に変換
-        /* onChange:ユーザーが選択を変更したときに呼ばれる関数
-          e.target.value:ユーザーが選択した値を取得
-          `e.target.value` の型を number[]（数値の配列）にinしている*/
-        onChange={(e) => handleChange(e.target.value as Category[])} // 型を明示
-        /*Material-UIのコンポーネントで、入力フィールドのスタイルを指定 
-        OutlinedInput:フィールドの周りにアウトライン（枠線）が表示されるスタイル*/
+        value={selectedCategories.map((category) => category.id)} // 現在選択されているカテゴリーのオブジェクト配列
+        // /* onChange:ユーザーが選択を変更したときに呼ばれる関数
+        // e.target.value:ユーザーが選択した値を取得
+        // `e.target.value` の型を number[]（数値の配列）にinしている*/
+        onChange={handleChange} // 型を明示
+        // /*Material-UIのコンポーネントで、入力フィールドのスタイルを指定
+        // OutlinedInput:フィールドの周りにアウトライン（枠線）が表示されるスタイル*/
         input={<OutlinedInput />}
-        renderValue={(selected: Category[]) => (
-          /*sx プロパティは、Material-UIのスタイルシステムを利用して、直接スタイルを指定するためのもの */
-          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-            {selected.map((value: Category) => (
-              /*Chip は、選択されたカテゴリーを表示するための小さなUI要素 */
-              <Chip key={value.id} label={value.name} />
-            ))}
-          </Box>
-        )}
+        renderValue={(selected) => {
+          if (selected.length === 0) {
+            return "カテゴリーを選択"; // 初期状態の表示
+          }
+          return (
+            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+              {selected.map((id) => {
+                const category = categories.find((c) => c.id === id);
+                return category ? (
+                  <Chip key={category.id} label={category.name} />
+                ) : null;
+              })}
+            </Box>
+          );
+        }}
       >
         {categories.map((category) => (
           //MenuItem は、ユーザーが選択できる項目を表示します。例えば、ドロップダウンメニューの中で、ユーザーがクリックして選ぶことができる項目など
---
+++ src/app/admin/categories/page.tsx
@@ -0,0 +1,66 @@
+//カテゴリー一覧ページ admin/categories
+
+"use client";
+
+import Link from "next/link"; //Next.jsでページ間を移動するときに使う道具.ウェブページのリンクを簡単に作ることができる
+import { useEffect, useState } from "react"; //useState:ボタンをクリックした回数を数えるような場合(表),useEffect:データの取得や、タイマーの設定など、コンポーネントの表示以外で何かを行うときに使う（裏）
+import { Post } from "@/app/types/Post"; //Postという型（型というのは、データの形を定義）をインポート
+import { Category } from "@/app/types/Category"; //`Category`という型をインポート(データの形を定義)
+
+export default function Page() {
+  //categories: 現在のカテゴリーのリストを保持するための変数
+  //Category[]: `Category`という型の配列であることを示す。カテゴリーのリストを表しています。
+  //([]): 初期状態として空の配列を設定しています。つまり、最初はカテゴリーが何もない状態
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  //useEffect:コンポーネントが画面に表示されたときに実行される処理を定義するためのフック
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch("/api/admin/categories"); //`/api/admin/categories`というURLからカテゴリーのデータを取得しています。
+      //JSONから受け取ったデータの中から`categories`というプロパティを取り出しています。
+      const { categories } = await res.json(); //`fetch`関数を使って取得したレスポンス（`res`）をJSON形式に変換
+      //`useState`フックで定義した`setCategories`関数を使って、取得した`categories`のデータをセットしています。
+      // categories`という状態が更新され、ページが再レンダリングされることで、最新のカテゴリー情報が画面に表示されます。
+      setCategories(categories);
+    };
+
+    fetcher(); //先ほど定義した`fetcher`という非同期関数を呼び出しています。これにより、実際にデータの取得と状態の更新が行われます。
+  }, []); //空の配列を渡すことで、この`useEffect`はコンポーネントの最初のレンダリング時に一度だけ実行される
+
+  //カテゴリーの一覧を表示するためのページを作成しています。
+  // ユーザーはカテゴリーの名前をクリックして、その詳細ページに移動することができます。
+  // また、新しいカテゴリーを追加するためのボタンも用意されています。
+
+  return (
+    <>
+      {/*justify-between: 子要素を両端に配置します。 */}
+      <div className="mb-8 flex items-center justify-between">
+        {/*text-xlは、文字のサイズを「エクストララージ（extra large）=20ピクセル（px）」に相当」に設定するクラス */}
+        <h1 className="text-xl font-bold">カテゴリー一覧</h1>
+        <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+          {/*href="/admin/categories/new": クリックすると新しいカテゴリーを作成するページに移動します */}
+          <Link href={"/admin/categories/new"}>新規作成</Link>
+        </button>
+      </div>
+
+      <div>
+        {/*categoriesの配列をループして、各カテゴリーを表示します。
+          categoryは配列の各要素を指します。 */}
+        {categories.map((category) => {
+          return (
+            //href={`/admin/categories/${category.id}`}:各カテゴリーをクリックすると、そのカテゴリーの詳細ページに移動
+            //key={category.id}: Reactが要素を効率的に更新するために必要な一意の識別子
+            <Link href={`/admin/categories/${category.id}`} key={category.id}>
+              {/*border-b border-gray-300: 下に灰色の境界線を引きます。 
+                cursor-pointer: マウスカーソルを指の形にします（クリックできることを示します）。*/}
+              <div className="cursor-pointer border-b border-gray-300 p-4 hover:bg-gray-100">
+                {/*category.name: カテゴリーの名前を表示するためのもの.各カテゴリーのデータから名前を取り出して表示*/}
+                <div className="text-xl font-bold">{category.name}</div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+}
